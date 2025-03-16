b9e95e66c1ee0461816301fff35d064a72bb720f
Marcelo Rossini <marcelo_rossini@outlook.com>
2025-03-16 02:32:30 +0000

    upload

--- src/components/ImageEditor/Handles.tsx
+++ src/components/ImageEditor/Handles.tsx
@@ -8,7 +8,7 @@ interface HandleProps {
 
 export const RotateHandle: React.FC<HandleProps> = ({ onMouseDown }) => (
     <div
-        className="rotate-handle absolute top-0 right-0 w-10 h-10 cursor-pointer opacity-10 hover:opacity-100 transition flex items-center justify-center"
+        className="rotate-handle absolute top-0 right-0 w-10 h-10 cursor-pointer opacity-10 hover:opacity-100 transition items-center justify-center hidden lg:flex [.no-touch_&]:flex"
         onMouseDown={onMouseDown}
     >
         <FaArrowRotateLeft size={28} />
@@ -17,7 +17,7 @@ export const RotateHandle: React.FC<HandleProps> = ({ onMouseDown }) => (
 
 export const ResizeHandle: React.FC<HandleProps> = ({ onMouseDown }) => (
     <div
-        className="resize-handle absolute bottom-0 right-0 w-10 h-10 cursor-pointer opacity-10 hover:opacity-100 transition flex items-center justify-center"
+        className="resize-handle absolute bottom-0 right-0 w-10 h-10 cursor-pointer opacity-10 hover:opacity-100 transition items-center justify-center hidden lg:flex [.no-touch_&]:flex"
         onMouseDown={onMouseDown}
     >
         <FaArrowRotateLeft size={28} />
